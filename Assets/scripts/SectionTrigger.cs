4af0076f96b3ea5fd9804019d89315bfe7c593ba
Quinten Woolderink <quintencoderen@gmail.com>
2024-04-10 08:40:17 +0200

    section trigger fixed

--- Assets/scripts/SectionTrigger.cs
+++ Assets/scripts/SectionTrigger.cs
@@ -6,12 +6,10 @@ using UnityEngine;
 public class SectionTrigger : MonoBehaviour
 {
     public GameObject world;
-    public static int instantiateCount = 1;
 
     private void OnTriggerEnter(Collider other)
     {
         var instantiatedWorld = Instantiate(world);
-        instantiatedWorld.transform.position += new Vector3(0, 0, 978) * instantiateCount;
-        instantiateCount++;
+        instantiatedWorld.transform.position = transform.parent.transform.position + new Vector3(0, 0, 978);
     }
 }
